cb4b78b8839c0c27db0dbae3a0fa8c91c537dccc
Cemil O <covacik@Cemils-MacBook-Pro.local>
2022-06-08 23:43:32 -0400

    problem 217 solution-2 added

--- 217-Contains-Duplicate.py
+++ 217-Contains-Duplicate.py
@@ -6,9 +6,18 @@ twice in the array, and return false if every element is distinct.
 class Solution:
     def containsDuplicate(self, nums:list[int]) -> bool:
         return len(nums)>len(set(nums))
+    def containsDuplicate2(self, nums:list[int]) -> bool:
+        hashset=set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
 
 
 s=Solution()
 print(s.containsDuplicate(nums=[1,2,3,4]))
 print(s.containsDuplicate(nums=[1,2,2,3,4]))
+print(s.containsDuplicate2(nums=[1,2,3,4]))
+print(s.containsDuplicate2(nums=[1,2,2,3,4]))
     
